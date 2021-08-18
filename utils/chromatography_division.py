6afd430d7e2d88a6f904d5f000069d4457515ad8
bnaylor51 <56124987+bnaylor51@users.noreply.github.com>
2021-08-18 12:29:08 -0600

    adjusted to load settings in multiprocessing
    in windows global variables are not passed between processors, so adjusted to load settings in every processor

--- utils/chromatography_division.py
+++ utils/chromatography_division.py
@@ -1,67 +1,40 @@
-# -*- coding: utf-8 -*-
-"""
-Copyright (c) 2021 Bradley Naylor, Michael Porter, Kyle Cutler, Chad Quilling, J.C. Price, and Brigham Young University
-All rights reserved.
-Redistribution and use in source and binary forms,
-with or without modification, are permitted provided
-that the following conditions are met:
-    * Redistributions of source code must retain the
-      above copyright notice, this list of conditions
-      and the following disclaimer.
-    * Redistributions in binary form must reproduce
-      the above copyright notice, this list of conditions
-      and the following disclaimer in the documentation
-      and/or other materials provided with the distribution.
-    * Neither the author nor the names of any contributors
-      may be used to endorse or promote products derived
-      from this software without specific prior written
-      permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-
 import pandas as pd
 import numpy as np
-from obs.peak import Peak
-from obs.id import ID
-from obs.envelope import Envelope
-from obs.molecule import Molecule
-import deuterater.settings as settings
+try:
+	from obs.peak import Peak
+	from obs.id import ID
+	from obs.envelope import Envelope
+	from obs.molecule import Molecule
+	import deuterater.settings as settings
+except:
+	from DeuteRater.obs.peak import Peak
+	from DeuteRater.obs.id import ID
+	from DeuteRater.obs.envelope import Envelope
+	from DeuteRater.obs.molecule import Molecule
+	import DeuteRater.deuterater.settings as settings
+from functools import partial
 import multiprocessing as mp
 import traceback
 import os
 from tqdm import tqdm
-from functools import partial
 
 class ChromatographyDivider:
 	
-	def __init__(self, settings_path, out_paths, input_paths):
+	def __init__(self, settings_path, out_paths, input_paths, biomolecule_type = "Peptide"):
 		self.settings_path = settings_path
 		settings.load(self.settings_path)
 		
 		self.input_paths = input_paths
 		self.out_paths = out_paths
 		self.how_divide = settings.use_chromatography_division
+		self.biomolecule_type = biomolecule_type
 		
 		try:
 			if settings.recognize_available_cores is True:
 				self._n_processors = mp.cpu_count()
 			else:
 				self._n_processors = settings.n_processors
-			#$breaks windows/python interactions if too many cores are used.  very niche application but still relevant
-			if self._n_processors > 60:
-				self.n_processors = 60
+			
 			self._mp_pool = mp.Pool(self._n_processors)
 		
 		except Exception as e:
@@ -178,10 +151,17 @@ class ChromatographyDivider:
 			molecule_groups = df.groupby(by=df.columns[0])
 			df["Extraction_Updated"] = ""
 			df["Extraction_Error"] = ""
-			func = partial(self.handle_molecule, settings_path=self.settings_path)
-            
-			molecules = self._mp_pool.map(func,
-										  tqdm(molecule_groups, desc="dividing chromatography: "))
+			
+			molecules = list()
+
+			if settings.debug_level == 0:
+				func = partial(self.handle_molecule, settings_path = self.settings_path)
+				molecules = self._mp_pool.map(func,
+											  tqdm(molecule_groups, desc="dividing chromatography: ", total=len(molecule_groups)))
+
+			elif settings.debug_level >= 1:
+				for group in tqdm(molecule_groups, desc="dividing chromatography: ", total=len(molecule_groups)):
+					molecules.append(self.handle_molecule(group, self.settings_path))
 			
 			df = pd.concat(molecules)
 			
@@ -201,13 +181,71 @@ class ChromatographyDivider:
 				molecule_groups = df.groupby(by=df.columns[0])
 				df["Extraction_Updated"] = ""
 				df["Extraction_Error"] = ""
-				func = partial(self.handle_molecule, settings_path=self.settings_path)
-				molecules = self._mp_pool.map(func,
-											  tqdm(molecule_groups,
-												   desc="dividing chromatography: ",
-												   total=len(molecule_groups),
-												   leave=False))
+				molecules = list()
+				
+				if settings.debug_level == 0:
+					func = partial(self.handle_molecule, settings_path = self.settings_path)
+					molecules = self._mp_pool.map(func,
+												  tqdm(molecule_groups,
+													   desc="dividing chromatography: ",
+													   total=len(molecule_groups),
+													   leave=False))
+				
+				elif settings.debug_level >= 1:
+					for group in tqdm(molecule_groups, desc="dividing chromatography: ", total=len(molecule_groups), leave=False):
+						molecules.append(self.handle_molecule(group, self.settings_path))
 				
 				df = pd.concat(molecules)
 				
 				df.to_csv(outfile, sep='\t', index=False)
+
+
+def main():
+	def tk_get_files(extension='*', prompt="Select file"):
+		from tkinter import filedialog
+		from tkinter import Tk
+		import os
+		root = Tk()
+		root.withdraw()
+		if (extension == '*'):
+			root.filename = filedialog.askopenfilenames(
+				initialdir=os.getcwd(), title=prompt)
+		else:
+			extension_list = list()
+			extension.split(",")
+			for extension in extension.split(","):
+				if extension == ' ':
+					continue
+				elif extension[0] == ' ':
+					extension = extension[1:]
+				elif extension[0] != '.':
+					extension = "." + extension
+				extension_list.append((extension + " Files", extension), )
+			extension_list.append(("All Files", "*"), )
+			
+			root.filename = filedialog.askopenfilenames(
+				initialdir=os.getcwd(), title=prompt,
+				filetypes=extension_list)
+		root.update()
+		
+		filename = root.filename
+		if len(filename) == 1:
+			return filename[0]
+		return filename  # A string representing the file path
+
+	settings_path = "../resources/settings.yaml"
+
+	input_files = tk_get_files()
+	if len(input_files[0]) == 1:
+		input_files = [input_files]
+	out_files = [f[:-4] + "_divided.tsv" for f in input_files]
+	
+	divider = ChromatographyDivider(settings_path=settings_path,
+									input_paths=input_files,
+									out_paths=out_files,
+									biomolecule_type="Lipids"
+									)
+	divider.divide()
+
+if __name__ == "__main__":
+	main()
